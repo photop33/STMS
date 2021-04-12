363ca15880699a3fb5fda4f0ef91de0c8169393a
photop33 <75907575+photop33@users.noreply.github.com>
2021-04-12 16:56:07 +0300

    Delete bootstrap.sh

--- bootstrap.sh
+++
@@ -1,3 +0,0 @@
-cp /etc/ansible/sample/handlers/main.yml /etc/ansible/roles/nginx/handlers/main.yml
-cp /etc/ansible/sample/tasks/main.yml /etc/ansible/roles/nginx/tasks/main.yml
-cp /etc/ansible/sample/server.yml /etc/ansible/server.yml
