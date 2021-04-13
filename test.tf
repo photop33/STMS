0f9b51e6877e3930cf9b35a985a270c73a3d6c8c
photop33 <75907575+photop33@users.noreply.github.com>
2021-04-13 19:39:50 +0300

    Create test.tf

---
+++ test.tf
@@ -0,0 +1,40 @@
+provider "aws" {
+  region = "us-east-2"
+  access_key = "AKIAYPAIJQJTHGFV7WPF"
+  secret_key = "m5mJojPkO/ctUWebQ77sZvtvj8LgUpNLHuEiPb1C"
+}
+
+resource "aws_instance" "example" {
+  ami           = " ami-01e7ca2ef94a0ae86"
+  instance_type = "t2.micro"
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo apt update
+              yes | sudo get-apt install ansible
+              yes | sudo get-apt install git 
+              EOF
+  
+  tags = {
+    Name = "terraform-example"
+  }
+}
+
+resource "aws_security_group" "instance" {
+
+    name = "terraform-example-instance"
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    }
+  provisioner "local-exec" {
+    command = "ansible-playbook playbook.yml" 
+}
+}
